4f7bb779a8de0ac8386bd26cf2d644e6e1d408a4
MayTheStar <maibadr200@gmail.com>
2024-10-28 18:57:33 +0300

    Initial Commit

---
+++ LearningChallang/LearningChallangApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningChallangApp.swift
+//  LearningChallang
+//
+//  Created by May Bader Alotaibi on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningChallangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningChallangTests/LearningChallangTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningChallangTests.swift
+//  LearningChallangTests
+//
+//  Created by May Bader Alotaibi on 25/04/1446 AH.
+//
+
+import Testing
+@testable import LearningChallang
+
+struct LearningChallangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
